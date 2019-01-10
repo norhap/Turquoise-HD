8e759a75006549c3e60691cbf5c69fb99c178783
norhap <alvaro@elfogondesegovia.com>
2019-01-10 19:36:42 +0100

    Update BTVInfo.py

--- usr/lib/enigma2/python/Components/Converter/BTVInfo.py
+++ usr/lib/enigma2/python/Components/Converter/BTVInfo.py
@@ -102,7 +102,7 @@ class BTVInfo(Poll, Converter, object):
                 uptime += str(hours) + ' ' + (hours == 1 and 'hora' or 'horas') + ', '
             if len(uptime) > 0 or minutes > 0:
                 uptime += str(minutes) + ' ' + (minutes == 1 and 'minuto' or 'minutos')
-            return 'Tiempo Actividad: %s' % uptime
+            return 'Time Activity: %s' % uptime
 
     def getTempSensor(self):
         if 'dm7020hd' not in HardwareInfo().get_device_name():
